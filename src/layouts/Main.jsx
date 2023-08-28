d3421e2d092757d3c9bffdeb884dfa1b53362eec
Hemel Bourhan <hemelbourhan@gmailgit config --global user.email Bourhan>
2023-08-29 01:18:57 +0600

    set header component and use moment js for dynamic time

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
-import Footer from '../pages/Shored/Footer/Footer';
-import Header from '../pages/Shored/Header/Header';
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import Footer from "../pages/Shored/Footer/Footer";
+import Header from "../pages/Shored/Header/Header";
+import LeftNav from "../pages/Shored/LeftNav";
+import RightNav from "../pages/Shored/RightNav/RightNav";
 
 const Main = () => {
-    return (
-        <div>
-            <Header></Header>
-
-            <Footer></Footer>
-        </div>
-    );
+  return (
+    <div>
+      <Header></Header>
+      <Container>
+        <Row>
+          <Col lg={3}>
+               <LeftNav></LeftNav>
+            </Col>
+          <Col lg={6}>
+            <h2>main content coming soon ...!</h2>
+          </Col>
+          <Col lg={3}>
+                <RightNav></RightNav>
+          </Col>
+        </Row>
+      </Container>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Main;
--- src/pages/Shored/Header/Header.jsx
+++ src/pages/Shored/Header/Header.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
-
+import moment from 'moment';
+import React from "react";
+import logo from "../../../assets/logo2.png";
 const Header = () => {
-    return (
-        <div>
-            <p>this is header page ...!</p>
-        </div>
-    );
+  return (
+    <div>
+      <div className="div text-center">
+        <img src={logo} alt="" />
+        <h5 className="mt-2">Your Source for Today's News, Tomorrow's Insights.</h5>
+        <h4>{moment().format("dddd, MMMM D, YYYY, h:mm:ss a")}</h4>
+
+      </div>
+    </div>
+  );
 };
 
 export default Header;
