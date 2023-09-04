39e6ab8d24dea95f66a77a4c9d5f00b3f25b786e
Hemel Bourhan <hemelbourhan@gmailgit config --global user.email Bourhan>
2023-09-05 00:00:42 +0600

    set the news card and show all the data from server

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -1,24 +1,54 @@
+import moment from "moment";
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Card, Image } from "react-bootstrap";
+import { FaBookmark, FaRegEye, FaShareAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const NewsCard = ({ news }) => {
-  const { _id,image_url, title, details } = news;
+  const { _id, image_url, title, total_view, author, published_date, details } =
+    news;
   return (
-    <div>
-      <Card className="text-center">
-        <Card.Header>Featured</Card.Header>
+    <>
+      <Card className="mt-2">
+        <Card.Header className="d-flex align-items-center">
+          <div className="">
+            <Image style={{ height: "60px" }} src={author.img} roundedCircle />
+          </div>
+          <div className="ps-3 flex-grow-1">
+            <h6 className="mb-0">{author?.name}</h6>
+            <p> {moment(author?.published_date).format("yyyy-mm-DD")}</p>
+          </div>
+          <div className="mx-2 d-flex gap-2">
+            <FaBookmark></FaBookmark>
+            <FaShareAlt></FaShareAlt>
+          </div>
+        </Card.Header>
         <Card.Body>
           <Card.Title>{title}</Card.Title>
           <Card.Img variant="top" src={image_url} />
           <Card.Text>
-            {details.length < 250 ? <>{details}</>: <>{details.slice(0,250)}... <Link to={`/news/${_id}`}>Read More</Link></>
-            }
+            {details.length < 250 ? (
+              <>{details}</>
+            ) : (
+              <>
+                {details.slice(0, 250)}...{" "}
+                <Link to={`/news/${_id}`} className="text-danger">
+                  Read More
+                </Link>
+              </>
+            )}
           </Card.Text>
         </Card.Body>
-        <Card.Footer className="text-muted">2 days ago</Card.Footer>
+        <Card.Footer className="text-muted align-items-center d-flex">
+          <div className="flex-grow-1">
+            <h1>gg</h1>
+          </div>
+          <div className="">
+            <FaRegEye></FaRegEye> {total_view}
+          </div>
+        </Card.Footer>
       </Card>
-    </div>
+    </>
   );
 };
 
