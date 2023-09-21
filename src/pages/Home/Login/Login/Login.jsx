fb0b5ab0ce6ed0e1f12b78b1720ce086149f4f9c
Hemel Bourhan <hemelbourhan@gmailgit config --global user.email Bourhan>
2023-09-22 01:03:53 +0600

    updated login component

--- src/pages/Home/Login/Login/Login.jsx
+++ src/pages/Home/Login/Login/Login.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Login = () => {
   return (
     
-     <Container className="mt-5 w-25">
+     <Container className="mt-5 w-50">
         <h4 className="text-center">Login your account </h4>
          <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -20,17 +20,14 @@ const Login = () => {
           <Form.Label>Password</Form.Label>
           <Form.Control type="password" placeholder="Password" required />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
         <Button className="w-100" variant="primary" type="submit">
           Login
         </Button>
         <br />
-        <p>Don't have account ? <span  ><Link className="text-decoration-none text-danger" to="/register">Register</Link></span></p>
+        <p>Don't have account ?<span  ><Link className="text-decoration-none text-danger" to="/register">Register</Link></span></p>
       </Form>
      </Container>
-    
+         
   );
 };
 
